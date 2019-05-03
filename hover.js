0797a792dcff35c559d2eb8caec02f3b34457aef
Jacob Day <jacob.m.day@icloud.com>
2019-05-02 21:34:05 -0500

    Update hover.js

--- hover.js
+++ hover.js
@@ -5,3 +5,64 @@ $(function() {
     $('.topnav').css('box-shadow', '');
   });
 });
+
+$(function() {
+  $('.btonagel').hover(function() {
+    $('#btonagel').css('height', '412px');
+    $('#btonagel').css('width', '208px');
+    $('#btonagel').css('opacity', '1');
+}, function() {
+    $('#btonagel').css('height', '0');
+    $('#btonagel').css('width', '0');
+    $('#btonagel').css('opacity', '0');
+
+  });
+});
+
+$(function() {
+  $('.supp').hover(function() {
+    $('#supp').css('height', '412px');
+    $('#supp').css('width', '208px');
+    $('#supp').css('opacity', '1');
+ }, function() {
+    $('#supp').css('height', '0');
+    $('#supp').css('width', '0');
+    $('#supp').css('opacity', '0');
+  });
+});
+
+$(function() {
+  $('.kstaats').hover(function() {
+    $('#kstaats').css('height', '412px');
+    $('#kstaats').css('width', '208px');
+    $('#kstaats').css('opacity', '1');
+ }, function() {
+    $('#kstaats').css('height', '0');
+    $('#kstaats').css('width', '0');
+    $('#kstaats').css('opacity', '0');
+  });
+});
+
+$(function() {
+  $('.cschafer').hover(function() {
+    $('#cschafer').css('height', '412px');
+    $('#cschafer').css('width', '208px');
+    $('#cschafer').css('opacity', '1');
+ }, function() {
+    $('#cschafer').css('height', '0');
+    $('#cschafer').css('width', '0');
+    $('#cschafer').css('opacity', '0');
+ });
+});
+
+$(function() {
+  $('.hwireman').hover(function() {
+    $('#hwireman').css('height', '412px');
+    $('#hwireman').css('width', '208px');
+    $('#hwireman').css('opacity', '1');
+}, function() {
+    $('#hwireman').css('height', '0');
+    $('#hwireman').css('width', '0');
+    $('#hwireman').css('opacity', '0');
+  });
+});
