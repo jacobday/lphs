892782b19ce5f1f24f255332c34a05c2c55273c6
Jacob Day <jacob.m.day@icloud.com>
2019-05-12 19:40:10 -0500

    Add files via upload

--- assets/js/hover.js
+++ assets/js/hover.js
@@ -11,6 +11,14 @@ $("a[href='#top']").click(function() {
   return false;
 });
 
+$(document).ready(function(){
+    var outerContent = $('.news-row');
+    var innerContent = $('.news-row > div');
+    
+    outerContent.scrollLeft(550);
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+});
+
 
 $(function() {
   $('.btonagel').hover(function() {
